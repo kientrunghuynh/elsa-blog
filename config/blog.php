b7057e4b3cca5f6f9849bfba5593cbcbfab68403
Trung Huynh <thy@tapptic.com>
2016-03-03 15:10:40 +0700

    initialize application

---
+++ config/blog.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'name' => "Elsa",
+    'title' => "Elsa - Blog",
+    'subtitle' => 'A clean blog written in Laravel 5.1',
+    'description' => 'This is my meta description',
+    'author' => 'Chuck Heintzelman',
+    'page_image' => 'home-bg.jpg',
+    'posts_per_page' => 10,
+    'uploads' => [
+        'storage' => 'local',
+        'webpath' => '/uploads/',
+    ],
+];
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+
+        <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.css')}}"/>
+
+        <script language="javascript" src="{{asset('js/jquery.min.js')}}"></script>
+        <script language="JavaScript" src="{{asset('js/bootstrap.min.js')}}"></script>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">Laravel 5</div>
+            </div>
+        </div>
+    </body>
+</html>
